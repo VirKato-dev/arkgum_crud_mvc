7ebd61f2ea9cb48914db758c6ef50497a294bb80
arkgum <arkgum@gmail.com>
2024-01-26 16:43:38 +0300

    Initial commit

---
+++ src/main/java/web/config/AppConfig.java
@@ -0,0 +1,10 @@
+package web.config;
+
+
+
+public class AppConfig  {
+
+
+
+
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,64 @@
+package web.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+    @Column(name="email")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
